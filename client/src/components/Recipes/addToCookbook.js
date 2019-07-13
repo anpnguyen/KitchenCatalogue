297bc22132efe2d31d34f3307e931673bc30a25d
PleatherShaman <shamanwebsolutions@gmail.com>
2019-07-13 12:31:15 +1000

    add selector for what cookbooks to update

--- client/src/components/Recipes/addToCookbook.js
+++ client/src/components/Recipes/addToCookbook.js
@@ -4,31 +4,53 @@ import "../Recipes/individualRecipe.css";
 import AddToCookbookSelect from "./addToCookbookSelect";
 
 function AddToCookbook(props) {
-  const { setIsFavourite, cookbook, recipeId } = props;
-  const { cookbooks } = cookbook;
+  const { setIsFavourite,  recipeId , cookbook} = props;
+  
   const [size, setSize] = useState(1);
 
-  const [cookbooksIn, setCookbooksIn] = useState();
+  const [addedCookbooks, setAddedCookbooks] = useState([]);
+
+ 
 
   const handleIsFavourite = () => {
     setIsFavourite(false);
   };
 
   const handleAddToCookbook = () => {
-    alert("add to cookbook");
+    let selectedCookbooks = addedCookbooks.map( addedCookbook => (addedCookbook.value))
+    console.log(selectedCookbooks)
+
+    let toAdd = selectedCookbooks.filter(cookbook => cookbook.savedRecipes.includes(recipeId) === false)
+
+    console.log(toAdd)
+
+
+
+    // recipe ID
+    // cookbook id
+    // take cookbook ID, loook in saved recipes, if present, do nothin - 
+    // if not present, add to mongo pUT requet
+
+    // let positiveCookbooks = cookbook.cookbooks.filter( individualCookbook => individualCookbook.savedRecipes.includes(recipeId) )
+    // this should print out all cookbook that have this 
+    // console.log(positiveCookbooks)
+
+
+
+    // 
+    // client side seach
+    
+    // add recipe to cookbooks
   };
 
-  useEffect(() => {
-    const result = cookbooks.filter(individualCookbook =>
-      individualCookbook.savedRecipes.includes(recipeId)
-    );
-    setCookbooksIn(result);
-  }, []);
+  
 
   return (
     <div className="addFavourite">
       <h2>Which cook book do you want to add this to?</h2>
-      <AddToCookbookSelect cookbooksIn={cookbooksIn} />
+      <AddToCookbookSelect 
+        setAddedCookbooks = {setAddedCookbooks}
+      />
 
       <div className={`${size === 5 ? "hideButtons" : ""} favouriteButtons`}>
         <button className="blueButton" onClick={handleAddToCookbook}>
--- client/src/components/Recipes/addToCookbookSelect.js
+++ client/src/components/Recipes/addToCookbookSelect.js
@@ -9,26 +9,36 @@ const animatedComponents = makeAnimated();
 
 const AddToCookbookSelect = (props)=> {
 
-    const {cookbook, match} = props
-    const [defaultCookbooks, setDefaultCookbooks] = useState();
+    const {cookbook, setAddedCookbooks} = props
+    
     const [selectOptions, setSelectOptions] = useState();
+    
+
+    const handleSelectChange = (e)=>{
+      
+      
+      setAddedCookbooks(e)
+      
+     
+    }
+
+    
 
     useEffect(() => {
-      const result = cookbook.cookbooks.filter(individualCookbook =>
-        individualCookbook.savedRecipes.includes(match.params.recipe_id)
-      );
+      // const result = cookbook.cookbooks.filter(individualCookbook =>
+      //   individualCookbook.savedRecipes.includes(match.params.recipe_id)
+      // );
       
-      const defaultOptions = result.map(defaultCookbook => ({value: defaultCookbook._id, label:defaultCookbook.cookbookTitle })) 
+      // const defaultOptions = result.map(defaultCookbook => ({value: defaultCookbook._id, label:defaultCookbook.cookbookTitle })) 
       // console.log(defaultOptions)
-      setDefaultCookbooks(defaultOptions)
+      // setDefaultCookbooks(defaultOptions)
 
 
       // cookbooksIn !== undefined && console.log(cookbooksIn)
 
-      let options = cookbook.cookbooks.map(individualCookbook => ({value: individualCookbook._id, label:individualCookbook.cookbookTitle }))
-      // console.log(options)
+      let options = cookbook.cookbooks.map(individualCookbook => ({value: individualCookbook, label:individualCookbook.cookbookTitle }))
       setSelectOptions(options)
-      console.log(options)
+      // console.log(options)
      
     }, []);
 
@@ -39,23 +49,22 @@ const AddToCookbookSelect = (props)=> {
 
 
     
-let options2 = [
-    { value: 'Color', label: 'Yellow' },
-    { value: 'Fruit', label: 'Apple' },
-    { value: 'Tool', label: 'Spanner' },
-  ]
+// let options2 = [
+//     { value: 'Color', label: 'Yellow' },
+//     { value: 'Fruit', label: 'Apple' },
+//     { value: 'Tool', label: 'Spanner' },
+//   ]
   return (
 
-    selectOptions !== undefined && defaultCookbooks!== undefined &&
+    selectOptions !== undefined  &&
     <Select
-      closeMenuOnSelect={false}
+      closeMenuOnSelect={true}
       components={animatedComponents}
-      
-      // defaultValue={[options[0], options[1]]}
-      defaultValue={defaultCookbooks}
+         
       isMulti
       options={selectOptions}
-    //   onChange={()=> alert('has been changed')}
+      onChange= {e=> handleSelectChange(e)}
+  
     />
   
   )
